bb43869da1260504ce198d6c2d0875f4f150f8b7
Thomas Viceconte <vicecontethomas@gmail.com>
2022-05-26 13:18:22 -0300

    Algoritmo utilizando un cilo

---
+++ app.js
@@ -0,0 +1,10 @@
+let numDeTabla = Number(prompt("Ingresa el número de la tabla de multiplicar"))
+    for(i = 1; i <=8; i++){
+        let res = numDeTabla * i;
+        alert("Resultado " + numDeTabla + "*" + i + " = " + res)
+}
+
+
+
+
+
