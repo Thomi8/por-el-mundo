0d07149c3dd14d7bb14f9d709a9df124db3be538
Thomas Viceconte <vicecontethomas@gmail.com>
2022-06-02 16:03:21 -0300

    simulador interactivo

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 let numDeTabla = Number(prompt("Ingresa el número de la tabla de multiplicar"))
-    for(i = 1; i <=8; i++){
+    for(i = 1; i <=5; i++){
         let res = numDeTabla * i;
         alert("Resultado " + numDeTabla + "*" + i + " = " + res)
 }
 
+function calculadora(primernumero, segundonumero, operacion) {
+    switch(operacion){
+        case "+":
+            return primernumero + segundonumero;
+        break;
+        case "-":
+            return primernumero - segundonumero;
+        break;
+        case "*":
+            return primernumero * segundonumero;
+        break;
+        case "/":
+            return primernumero / segundonumero;
+        break;
+        default:
+            return "Valores nulos";
+            break;
+    }
+}
 
+let pregunta1 = Number (prompt("Ingresa el primer num"))
+let pregunta2 = Number (prompt("Ingresa el segundo num"))
+let operadorMat = prompt("Ingresa + para sumar, - para restar, * para multiplicar, / para dividir");
 
-
-
+console.log(calculadora(pregunta1, pregunta2, operadorMat))
