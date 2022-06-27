355c152097f8a54ada289269d512a0ae7fd526a3
Thomas Viceconte <vicecontethomas@gmail.com>
2022-06-27 13:40:56 -0300

    JSON y storage

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-let usuario1 = prompt("Ingresa tu nombre y apellido");
+// let usuario1 = prompt("Ingresa tu nombre y apellido");
 
-if (usuario1 == "Thomas Viceconte" || usuario1 == "Natalia Ursino") {
-    alert("Hola " + usuario1 + " Bienvenido a Por el Mundo");
-    console.log("Puede ingresar");
-}else {
-    alert("Error: Ingrese los datos");
-    console.log("No puede ingresar");
-}
+// if (usuario1 == "Thomas Viceconte" || usuario1 == "Natalia Ursino") {
+//     alert("Hola " + usuario1 + " Bienvenido a Por el Mundo");
+//     console.log("Puede ingresar");
+// }else {
+//     alert("Error: Ingrese los datos");
+//     console.log("No puede ingresar");
+// }
 
 let numDeTabla = Number(prompt("Ingresa el número de la tabla de multiplicar"))
     for(i = 1; i <=8; i++){
@@ -62,3 +62,52 @@ boton.addEventListener("click", respuestaClick)
 function respuestaClick(){
     console.log("Email enviado");
 }
+
+
+//STORAGE 
+localStorage.setItem("nombre", "thomas");
+localStorage.setItem("apellido", "viceconte");
+localStorage.setItem("edad", "20");
+
+let nombre = localStorage.getItem("nombre");
+console.log(nombre);
+let apellido = localStorage.getItem("apellido");
+console.log(apellido);
+let edad = localStorage.getItem("edad");
+console.log(edad);
+
+localStorage.setItem("objeto", JSON.stringify({nombre: "bruno", apellido: "simone"}));
+let objeto = JSON.parse (localStorage.getItem("objeto"));
+console.log(objeto);
+
+
+// sessionStorage.setItem("usuario","thomiviceconte");
+
+const productos = [
+{destino:"miami", precio: 80000},
+{destino:"losangeles", precio: 50000},
+{destino:"cancun", precio: 60000},
+{destino:"riodejaneiro", precio: 40000},
+];
+
+const guardar = (clave, valor) => {localStorage.setItem(clave, valor)};
+for(const producto of productos){
+    guardar(producto.destino, JSON.stringify(producto));
+}
+
+
+let usuario3;
+let usuarioStorage = sessionStorage.getItem("usuario");
+
+if(usuarioStorage){
+    let message = `Bienvenido ${usuarioStorage}`;
+    alert(message);
+    usuario = usuarioStorage;
+}else{
+    alert("no iniciaste sesión");
+    usuario = prompt("Ingrese su nombre");
+    sessionStorage.setItem("usuario", usuario);
+}
+
+
+
